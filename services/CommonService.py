487a524af5494323abdc73450c87f28de4a6d97f
王志杰 <wzjwhtur@126.com>
2017-04-30 16:45:45 +0800

    no message

--- services/CommonService.py
+++ services/CommonService.py
@@ -14,7 +14,8 @@ from models.AssetTradeModel import AssetTrade
 from models.AssetTradeRetModel import AssetTradeRet
 from models.CashModel import Cash
 from models.CommonModel import session_deco
-from services.FeeService import get_daily_fee
+from models.DailyFeeModel import DailyFee
+# from services.FeeService import get_daily_fee, get_daily_fee_last_total_amount_by_date
 from utils import StaticValue as SV
 
 
@@ -568,5 +569,34 @@ def get_total_evaluate_detail(days=0):
     return ret
 
 
+@session_deco
+def get_daily_fee_last_total_amount_by_date(cal_date=date.today(), **kwargs):
+    session = kwargs.get(SV.SESSION_KEY, None)
+    ret = session.query(DailyFee, func.max(DailyFee.time).label('max_time')).filter(
+        DailyFee.is_active,
+        DailyFee.date == cal_date
+    ).one()
+
+    return ret.DailyFee.total_amount if ret.DailyFee else 0.0
+
+
+def add_fee_with_date(cal_date=date.today(), amount=0.0):
+    save(
+        DailyFee(
+            cal_date=cal_date,
+            amount=get_daily_fee_last_total_amount_by_date(cal_date=cal_date) + amount
+        )
+    )
+
+
+def get_daily_fee(cal_date=date.today()):
+    fees = query(DailyFee).filter(DailyFee.date == cal_date)
+    if fees.count() > 0:
+        return fees[-1].amount
+    else:
+        return 0.0
+
+
 if __name__ == '__main__':
-    print(get_total_evaluate_detail())
+    add_fee_with_date(cal_date=date.today(), amount=10000)
+    # print(get_total_evaluate_detail())
