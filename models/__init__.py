33173ae64a06ef15edacf17d1235eb2c9c1f5361
王志杰 <wzjwhtur@126.com>
2017-04-30 16:28:24 +0800

    daily fee

--- models/__init__.py
+++ models/__init__.py
@@ -3,14 +3,14 @@
 from __future__ import unicode_literals
 
 from models import CashModel, AssetClassModel, AssetFeeModel, AssetTradeModel, AssetFeeRateModel, AssetTradeRetModel, \
-    AssetRetRateModel
+    AssetRetRateModel, DailyFeeModel
 from models.CommonModel import init_db
 
 
 def models_all():
     result = []
     models = [AssetClassModel, CashModel, AssetTradeModel, AssetFeeModel, AssetFeeRateModel, AssetTradeRetModel,
-              AssetRetRateModel]
+              AssetRetRateModel, DailyFeeModel]
     map(lambda m: result.append(m.__all__), models)
     return result
 
