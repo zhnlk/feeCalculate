7552404d1b717576c62390e6660bc8572e46d560
zhnlk <tomleader0828@gmail.com>
2017-05-10 12:01:15 +0800

    GUI fix add filter bar

--- view/cashView/CashMain.py
+++ view/cashView/CashMain.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+import datetime
 from collections import OrderedDict
 
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QAction, QMainWindow, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QWidget
 from PyQt5.QtWidgets import QApplication
 
 from view.BasicWidget import BASIC_FONT, BasicFcView, BasicCell, NumCell
@@ -10,8 +10,67 @@ from controller.EventType import EVENT_CASH
 from controller.MainEngine import MainEngine
 
 
+class CashViewMain(BasicFcView):
+    def __init__(self, mainEngine, parent=None):
+        super(CashViewMain, self).__init__()
+        self.mainEngine = mainEngine
+        self.setMinimumSize(1300, 600)
+        self.initMain()
+
+    def initMain(self):
+        """初始化界面"""
+        self.setWindowTitle('现金主界面')
+        filterBar = FilterBar(self.mainEngine)
+        cashListView = CashListView(self.mainEngine)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(filterBar)
+        vbox.addWidget(cashListView)
+        self.setLayout(vbox)
+
+    def show(self):
+        """显示"""
+        super(CashViewMain, self).show()
+
+
+class FilterBar(QWidget):
+    def __init__(self, mainEngine, parent=None):
+        super(FilterBar, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        # 过滤的开始时间
+        filterStartDate_Label = QLabel('开始时间')
+        # 开始时间输入框
+        self.filterStartDate_Edit = QLineEdit(str(datetime.date.today()))
+        self.filterStartDate_Edit.setMaximumWidth(80)
+        # 过滤的结束时间
+        filterEndDate_Label = QLabel('结束时间')
+        # 结束时间输入框
+        self.filterEndDate_Edit = QLineEdit(str(datetime.date.today()))
+        self.filterEndDate_Edit.setMaximumWidth(80)
+
+        # 筛选按钮
+        filterBtn = QPushButton('筛选')
+        # 导出按钮
+        outputBtn = QPushButton('导出')
+
+        filterHBox = QHBoxLayout()
+        filterHBox.addStretch()
+        filterHBox.addWidget(filterStartDate_Label)
+        filterHBox.addWidget(self.filterStartDate_Edit)
+        filterHBox.addWidget(filterEndDate_Label)
+        filterHBox.addWidget(self.filterEndDate_Edit)
+
+        filterHBox.addWidget(filterBtn)
+        filterHBox.addWidget(outputBtn)
+
+        self.setLayout(filterHBox)
+
+
 class CashListView(BasicFcView):
     """现金详情"""
+
     def __init__(self, mainEngine, parent=None):
         """Constructor"""
         super(CashListView, self).__init__(parent=parent)
@@ -44,17 +103,19 @@ class CashListView(BasicFcView):
     def initUi(self):
         """初始化界面"""
         self.setWindowTitle('现金明细')
-        self.setMinimumSize(1300, 600)
+        # self.setMinimumSize(1300, 600)
 
         self.setFont(BASIC_FONT)
+
+        # self.initFilterBar()
         self.initTable()
         self.addMenuAction()
+        self.refresh()
 
         # 将信号连接到refresh函数
         self.signal.connect(self.refresh)
         self.mainEngine.eventEngine.register(self.eventType, self.signal.emit)
 
-    # ----------------------------------------------------------------------
     def showCashListDetail(self):
         """显示现金记录明细"""
 
@@ -99,7 +160,7 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     mainEngine = MainEngine()
-    clv = CashListView(mainEngine)
-
+    # clv = CashListView(mainEngine)
+    clv = CashViewMain(mainEngine)
     clv.show()
     sys.exit(app.exec_())
