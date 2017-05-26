3cb5c71bb44d16333d8b760663b78657c2cd7921
zhnlk <tomleader0828@gmail.com>
2017-05-26 13:11:09 +0800

    fix

--- view/cashView/CashMain.py
+++ view/cashView/CashMain.py
@@ -133,8 +133,8 @@ class CashViewMain(BasicFcView):
                 content = r[header]
                 cellType = self.cashListView.headerDict[header]['cellType']
                 cell = cellType(content)
-                if self.cashListView.saveData  and header is 'cal_date':
-                    cell.data = r[header]
+                if self.cashListView.saveData:
+                    cell.data = r['cal_date']
                 self.cashListView.setItem(row, n, cell)
             row = row + 1
 
@@ -195,7 +195,7 @@ class CashListViewWidget(BasicFcView):
 
     def doubleClickTrigger(self, cell):
         """根据单元格的数据撤单"""
-        self.widgetDict['showDataModifyView'] = CashDataModifyView(self.mainEngine,cell.data)
+        self.widgetDict['showDataModifyView'] = CashDataModifyView(self.mainEngine, cell.data)
         self.widgetDict['showDataModifyView'].show()
 
 
